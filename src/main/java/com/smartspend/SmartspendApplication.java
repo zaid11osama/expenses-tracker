535235796f960cef101462f7246833b09aacfc53
zaid11osama <zaidosama235@gmail.com>
2025-10-20 21:45:34 +0300

    ..

---
+++ src/main/java/com/smartspend/SmartspendApplication.java
@@ -0,0 +1,14 @@
+package com.smartspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartspendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartspendApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/com/smartspend/SmartspendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartspend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartspendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
